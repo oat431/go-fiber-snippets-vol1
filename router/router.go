45351530b93e55ab38f691456a94aaca9ad7e10c
oat431 <oat431@gmail.com>
2026-02-05 22:13:25 +0700

    add: health check for fiber

--- router/router.go
+++ router/router.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/healthcheck"
 )
 
 func init() {
@@ -12,6 +13,8 @@ func init() {
 
 func StartServer() {
 	app := fiber.New()
+	app.Get(healthcheck.LivenessEndpoint, healthcheck.New())
+	app.Get("/health", healthcheck.New())
 	err := app.Listen(":8080")
 	if err != nil {
 		return
