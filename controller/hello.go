ec6c14e3c7b4c4ba467a6af6f737f9ecd9325f5b
oat431 <oat431@gmail.com>
2026-02-11 23:17:14 +0700

    remove: unused comment

--- controller/hello.go
+++ controller/hello.go
@@ -13,11 +13,6 @@ func HelloWorld(c fiber.Ctx) error {
 		Status: common.SUCCESS,
 		Error:  nil,
 	}
-	//log.Info("📝 LOG: %s %s | Status: %d \n",
-	//	c.Method(),                // เช่น GET
-	//	c.Path(),                  // เช่น /health
-	//	c.Response().StatusCode(), // เช่น 200
-	//)
 	log.Info(c.Method() + " " + c.Path() + "\n")
 	return c.JSON(response)
 }
